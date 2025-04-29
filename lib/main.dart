58a01772ca035bb5f18f720835dcc352fa2cf4c5
neil warner <njwandroid@gmail.com>
2025-04-29 13:39:16 -0500

    Initial commit with one screen file that uses api and runs

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: QuotePage());
+}
+
+class QuotePage extends StatefulWidget {
+  @override
+  State<QuotePage> createState() => _QuotePageState();
+}
+
+class _QuotePageState extends State<QuotePage> {
+  bool _loading = true;
+  var _q = '';
+  @override
+  void initState() {
+    _fetch();
+  }
+
+  Future<void> _fetch() async {
+    setState(() => _loading = true);
+    final r = await http.get(Uri.parse('https://zenquotes.io/api/random'));
+    final d = jsonDecode(r.body);
+    setState(() {
+      _q = d.first['q'];
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(body: Center(child: _loading ? const CircularProgressIndicator() : Text(_q)));
+}
